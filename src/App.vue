b4f875b8184bd25566c1d6316b66ffe0e317c2f4
Ludwing Rivera A <ludwing.ra@gmail.com>
2019-04-23 12:31:40 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+#app
+    img(src='./assets/logo.png')
+    h1 MegaMusic
+    select(v-model="selectedCountry")
+        option(v-for="country in countries" v.bind:value="contry.value") {{ country.name }}
+    spinner(v-show="loading")
+    ul
+        artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+
+
+</template>
+
+<script>
+import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
+import getArtists from './api/index'
+
+export default {
+    name: 'app',
+    data () {
+        return {
+            artists: [],
+            countries: [
+            { name: 'Argentina', value: 'argentina' },
+            { name: 'Colombia', value: 'colombia' },
+            { name: 'Australia', value: 'australia' },
+            ],
+            selectedCountry: 'colombia',
+            loading: true
+        }
+    },
+    components: {
+        Artist: Artist,
+        Spinner
+    },
+    methods: {
+        refreshArtists(){
+            const self = this
+            this.loading = true
+            this.artists = []
+            getArtists(this.selectedCountry)
+                .then(function (resArtists) {
+                    self.loading = false
+                    self.artists = resArtists
+                })
+        }
+    },
+    mounted() {
+        this.refreshArtists()
+    },
+    watch: {
+        selectedCountry(){
+            this.refreshArtists()
+        }
+    }
+}
+</script>
+
+<style lang="stylus">
+    #app
+        font-family 'Avenir', Helvetica, Arial, sans-serif
+        -webkit-font-smoothing antialiased
+        -moz-osx-font-smoothing grayscale
+        text-align center
+        color #2c3e50
+        margin-top 60px
+    h1, h2
+        font-weight normal
+    ul
+        list-style-type none
+        padding 0
+    li
+        display inline-block
+        margin 0 10px
+    a
+        color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+
+export default {
+    appname: 'megamusic',
+    apikey: 'b603517d905a47480420dec88cb3c203',
+    secret: 'b7ab0d1fcb2324440804203d4f40a2f0',
+    registeredTo: 'ludwingra2'
+}
+
+
+
+//Application name	megamusic
+//API key	b603517d905a47480420dec88cb3c203
+//Shared secret	b7ab0d1fcb2324440804203d4f40a2f0
+//Registered to	ludwingra2
